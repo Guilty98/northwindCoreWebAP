6940267db8b84617fde9143d6a1db2598aca0d6e
Guilty98 <108742672+Guilty98@users.noreply.github.com>
2022-07-20 19:41:20 +0300

    NorthwindAPI
    api

---
+++ TryProject/WebAPITest/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entites.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("add")]
+        public IActionResult Get()
+        {
+           
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
